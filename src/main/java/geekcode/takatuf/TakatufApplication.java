10d195c4d8ca2582f5130158b86059d32280505a
Kamal Bdran <khajbdran@gmail.com>
2025-04-05 16:40:33 +0300

    Initial commit

---
+++ src/main/java/geekcode/takatuf/TakatufApplication.java
@@ -0,0 +1,13 @@
+package geekcode.takatuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakatufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakatufApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geekcode/takatuf/TakatufApplicationTests.java
@@ -0,0 +1,13 @@
+package geekcode.takatuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakatufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
