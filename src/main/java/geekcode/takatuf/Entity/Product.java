8f71b07ef9bfcddc35f91b76d09db44131e2946a
Kamal Bdran <khajbdran@gmail.com>
2025-04-09 01:29:59 +0300

    entity FIX errors and completing

--- src/main/java/geekcode/takatuf/Entity/Product.java
+++ src/main/java/geekcode/takatuf/Entity/Product.java
@@ -2,7 +2,9 @@ package geekcode.takatuf.Entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import java.sql.Timestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
@@ -21,7 +23,8 @@ public class Product {
 
     private String name;
     private String description;
-    private String piece;
+    private Integer piece;
+    private BigDecimal price;
     private String image;
     private String category;
     private String color;
--- src/main/java/geekcode/takatuf/Entity/Store.java
+++ src/main/java/geekcode/takatuf/Entity/Store.java
@@ -2,8 +2,8 @@ package geekcode.takatuf.Entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import java.sql.Timestamp;
 import java.util.List;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "stores")
