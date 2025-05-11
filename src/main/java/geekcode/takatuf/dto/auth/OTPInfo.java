7af8336ebd313aec8c28a9119fa4a89b2c013038
ghram <ghramalhajyalhajy@gmail.com>
2025-05-11 22:07:57 +0300

    -fix issues

---
+++ src/main/java/geekcode/takatuf/dto/auth/OTPInfo.java
@@ -0,0 +1,19 @@
+package geekcode.takatuf.Entity;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OTPInfo {
+    private final String otp;
+    private final LocalDateTime createdAt;
+    private boolean verified;
+
+    public OTPInfo(String otp, LocalDateTime createdAt, boolean verified) {
+        this.otp = otp;
+        this.createdAt = createdAt;
+        this.verified = verified;
+    }
+}
