2f6c466ba06293cf3635e5203da8f63f95ea4b58
ghram <ghramalhajyalhajy@gmail.com>
2025-05-11 20:39:21 +0300

    -fix issues

--- src/main/java/geekcode/takatuf/Repository/ComplaintRepository.java
+++ src/main/java/geekcode/takatuf/Repository/ComplaintRepository.java
@@ -3,6 +3,7 @@ package geekcode.takatuf.Repository;
 import geekcode.takatuf.Entity.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
