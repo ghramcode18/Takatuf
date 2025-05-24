fb5cd251bb96ef0e5cf51a4820521983d4c97a96
Kamal Bdran <khajbdran@gmail.com>
2025-05-24 13:34:59 +0300

    add every thing about orders and reviews

--- src/main/java/geekcode/takatuf/Security/SecurityConfig.java
+++ src/main/java/geekcode/takatuf/Security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .requestMatchers("/api/**").permitAll()
+               .requestMatchers("/api/auth/**").permitAll() 
                 // .requestMatchers("/api/user/**","/api/store/**","/api/product/**","/api/orders/**").authenticated()
                 .anyRequest().authenticated()
                 .and()
