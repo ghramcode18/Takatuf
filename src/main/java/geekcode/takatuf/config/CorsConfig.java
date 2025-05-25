0cebd02bcf7f26f92c940ed0d19aa18dc7a30024
ghram <ghramalhajyalhajy@gmail.com>
2025-05-25 21:04:15 +0300

    -fix issues with deployment and delete unused lib

--- src/main/java/geekcode/takatuf/config/CorsConfig.java
+++ src/main/java/geekcode/takatuf/config/CorsConfig.java
@@ -8,20 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:56790/",
-                                "https://teal-gorilla-464304.hostingersite.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept")
-                        .exposedHeaders("Access-Control-Allow-Headers", "Authorization")
-                        .allowCredentials(true);
-
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
+
