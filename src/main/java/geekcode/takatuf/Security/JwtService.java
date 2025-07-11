455d74fe223f4fc7e794323c9f16e0fd2fe92dd1
ghram <ghramalhajyalhajy@gmail.com>
2025-07-11 16:15:25 +0300

    -change the expiration for the token

--- src/main/java/geekcode/takatuf/Security/JwtService.java
+++ src/main/java/geekcode/takatuf/Security/JwtService.java
@@ -19,7 +19,7 @@ public class JwtService {
     @Value("${jwt.secret}")
     private String secretKey;
 
-    private static final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 15; // 15 minute
+    private static final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 60 * 24; // 1 day
     private static final long REFRESH_TOKEN_EXPIRATION = 1000 * 60 * 60 * 24 * 7; // 7 days
 
     private Key getSigningKey() {
