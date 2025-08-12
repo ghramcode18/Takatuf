7f264621b0688c6914f3f42ec1993d3263e2b571
Kamal Bdran <khajbdran@gmail.com>
2025-08-12 21:41:07 +0300

    fix HOME API

--- src/main/java/geekcode/takatuf/Service/HomeService.java
+++ src/main/java/geekcode/takatuf/Service/HomeService.java
@@ -17,24 +17,24 @@ public class HomeService {
     private final SliderService sliderService;
 
     public HomeResponse getHomeData() {
-        List<SectionResponse> sections = sectionService.getAllSections();
+        List<SectionResponse> sections = sectionService.getAllSections().stream()
+                .filter(s -> Boolean.TRUE.equals(s.getActive()))
+                .sorted(Comparator.comparing(
+                        SectionResponse::getSortOrder,
+                        Comparator.nullsLast(Integer::compareTo)))
+                .toList();
 
-        Map<String, List<SectionResponse>> groupedSections = sections.stream()
-                .collect(Collectors.groupingBy(section -> section.getType().toUpperCase()));
+        Map<String, List<SectionResponse>> grouped = sections.stream()
+                .collect(Collectors.groupingBy(s -> s.getType().toUpperCase()));
 
-        List<Map<String, Object>> resultSections = new ArrayList<>();
-
-        groupedSections.forEach((type, sectionList) -> {
-            List<Object> allData = sectionList.stream()
-                    .flatMap(s -> s.getData().stream())
-                    .collect(Collectors.toList());
-
-            Map<String, Object> sectionObject = new HashMap<>();
-            sectionObject.put("type", type);
-            sectionObject.put("data", allData);
-
-            resultSections.add(sectionObject);
-        });
+        List<Map<String, Object>> resultSections = grouped.entrySet().stream()
+                .map(e -> {
+                    Map<String, Object> m = new HashMap<>();
+                    m.put("type", e.getKey());
+                    m.put("data", e.getValue());
+                    return m;
+                })
+                .toList();
 
         List<SliderResponse> sliders = sliderService.getAllSliders();
 
@@ -43,4 +43,5 @@ public class HomeService {
                 .sections(resultSections)
                 .build();
     }
+
 }
